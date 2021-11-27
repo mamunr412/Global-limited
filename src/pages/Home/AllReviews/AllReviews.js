a46bbfc03a1227ba5d37e778841bade34b9a59fc
unknown <mamunr412@gmail.com>
2021-11-27 15:43:07 +0600

    c

--- src/pages/Home/AllReviews/AllReviews.js
+++ src/pages/Home/AllReviews/AllReviews.js
@@ -1,42 +1,40 @@
-import React,  { Component } from 'react';
-import { Typography } from '@mui/material';
-import { Box } from '@mui/system';
-import Container from '@mui/material/Container';
-import Carousel from 'react-elastic-carousel';
-import { useState } from 'react';
-import { useEffect } from 'react';
-import SingleReview from '../SingleReview/SingleReview';
+import React, { Component } from "react";
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import Container from "@mui/material/Container";
+import Carousel from "react-elastic-carousel";
+import { useState } from "react";
+import { useEffect } from "react";
+import SingleReview from "../SingleReview/SingleReview";
 
-const breakPoints=[
-{width:1, itemsToShow:1},
-{width:550, itemsToShow:2},
-
-]
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  { width: 550, itemsToShow: 2 },
+];
 
 const AllReviews = () => {
-    const [allReviews, setAllReviews] = useState([]);
-    useEffect(() => {
-        fetch('https://lit-fjord-60113.herokuapp.com/reviews')
-            .then(res => res.json())
-            .then(data => setAllReviews(data))
-    }, [])
-    return (
-        <div style={{ backgroundColor: "#f2f4f5" }}>
-            <Typography sx={{ my: 5 }} variant="h3" gutterBottom component="div">
-                Customers Review
-            </Typography>
-            <Box sx={{ flexGrow: 1 }}>
-                <Container >
-                    <Carousel breakPoints={breakPoints}>
-                        {allReviews.map(review => <SingleReview key={review._id}
-                            review={review}
-                        ></SingleReview>)}
-                    </Carousel>
-                </Container>
-            </Box>
-
-        </div>
-    );
+  const [allReviews, setAllReviews] = useState([]);
+  useEffect(() => {
+    fetch("https://lit-fjord-60113.herokuapp.com/reviews")
+      .then((res) => res.json())
+      .then((data) => setAllReviews(data));
+  }, []);
+  return (
+    <div style={{ backgroundColor: "#f2f4f5" }}>
+      <Typography sx={{ my: 5 }} variant="h3" gutterBottom component="div">
+        Customers Review
+      </Typography>
+      <Box sx={{ flexGrow: 1 }}>
+        <Container>
+          <Carousel breakPoints={breakPoints}>
+            {allReviews.map((review) => (
+              <SingleReview key={review._id} review={review}></SingleReview>
+            ))}
+          </Carousel>
+        </Container>
+      </Box>
+    </div>
+  );
 };
 
 export default AllReviews;
--- src/pages/Home/SingleReview/SingleReview.js
+++ src/pages/Home/SingleReview/SingleReview.js
@@ -1,33 +1,26 @@
-import React from 'react';
+import React from "react";
 
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import Rating from '@mui/material/Rating';
-
-
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Rating from "@mui/material/Rating";
 
 const SingleReview = ({ review }) => {
-    return (
-        <Card sx={{width:'95%',height:'250px'}} >
-            <CardContent>
-                <Typography variant="h5" component="div">
-                    {review.name}
-                </Typography>
-                <Typography variant="h6" component="div">
-                    {review.email}
-                </Typography>
-                <Typography variant="h6" component="div">
-                    <Rating name="read-only" value={review.rating} readOnly />
-                </Typography>
-                <Typography variant="body2" sx={{ mx:'20px',wordWrap:'break-word'}}>
-                    {review.text}
-                </Typography>
-            </CardContent>
-
-        </Card >
-
-    );
+  return (
+    <Card sx={{ width: "95%", height: "250px" }}>
+      <CardContent>
+        <Typography variant="h5" component="div" sx={{ mb: 2 }}>
+          {review.name}
+        </Typography>
+        <Typography variant="h6" component="div">
+          <Rating name="read-only" value={review.rating} readOnly />
+        </Typography>
+        <Typography variant="body2" sx={{ mx: "20px", wordWrap: "break-word" }}>
+          {review.text}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default SingleReview;
